40a576e6c4fa989c1a458bb5a2ee6fa1a5fffab5
mcguffin <joern@flyingletters.com>
2018-06-16 11:48:53 +0200

    Release version 0.0.3

--- include/AcfCustomizer/Compat/ACF/ACF.php
+++ include/AcfCustomizer/Compat/ACF/ACF.php
@@ -16,33 +16,25 @@ class ACF extends Core\PluginComponent {
 
 	protected function __construct() {
 
-		Customize::instance();
 
 		// need to figure out which version ist lowest...
 		if ( function_exists('acf') && version_compare( acf()->version, '5.6','>=' ) ) {
+
+			global $wp_customize;
+
+			if ( ! is_null( $wp_customize ) ) {
+				// instantinate at 11
+				add_action( 'plugins_loaded', array('ACFCustomizer\Compat\ACF\Customize','instance'), 11 );
+
+			}
+
 			require_once ACF_CUSTOMIZER_DIRECTORY . '/include/api/acf-functions.php';
+
 			acf_register_location_rule( 'ACFCustomizer\Compat\ACF\Location\Customizer' );
 
-			add_filter('acf/pre_load_value', array( $this, 'pre_load_value' ), 10, 3 );
-//			Location\Customizer::instance();
 		}
 	}
-	public function pre_load_value( $value, $post_id, $field ) {
-		if ( 'theme_mod' === $post_id ) {
-			$field = acf_get_valid_field( $field );
-			// get fieldgroup.
-			// get section
-			// get_theme_mod( section + '_' + fieldgroup )
 
-
-			// $field_group = $field;
-			// while ( $field_group['parent'] ) {
-			// 	$field_group =
-			// }
-			// var_dump();
-		}
-		return $value;
-	}
 	/**
 	 *	@inheritdoc
 	 */
