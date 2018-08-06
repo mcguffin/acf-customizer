78b9768c298197392a14f9cffc18763c7ce5ee68
mcguffin <joern@flyingletters.com>
2018-08-06 10:29:46 +0200

    Release version 0.0.8

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: ACF Customizer
 Plugin URI: https://github.com/mcguffin/acf-customizer
 Description: Use ACF Fields in customizer.
 Author: Jörn Lund
-Version: 0.0.7
+Version: 0.0.8
 Author URI:
 License: GPL3
 Github Repository: mcguffin/acf-customizer
