7298e7d73bb4266bf5cfd2c8384a993e6bc1d7ba
mcguffin <joern@flyingletters.com>
2019-01-16 17:53:51 +0100

    gulp err handling

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,14 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
+function swallowError (error) {
+
+	// If you want details of the error in the console
+	console.log(error.toString())
+
+	this.emit('end')
+}
+
 function do_scss( src ) {
 	var dir = src.substring( 0, src.lastIndexOf('/') );
 	return gulp.src( './src/scss/' + src + '.scss' )
@@ -28,7 +36,7 @@ function do_js( src, dir ) {
 	return gulp.src( './src/js/' + src + '.js' )
 		.pipe( sourcemaps.init() )
 		.pipe( gulp.dest( './js/' + dir ) )
-		.pipe( uglify().on('error', gulputil.log ) )
+		.pipe( uglify().on('error', swallowError ) )
 		.pipe( rename( { suffix: '.min' } ) )
 		.pipe( sourcemaps.write() )
 		.pipe( gulp.dest( './js/' + dir ) );
@@ -39,7 +47,7 @@ function concat_js( src, dest ) {
 		.pipe( sourcemaps.init() )
 		.pipe( concat( dest ) )
 		.pipe( gulp.dest( './js/' ) )
-		.pipe( uglify().on('error', gulputil.log ) )
+		.pipe( uglify().on('error', swallowError ) )
 		.pipe( rename( { suffix: '.min' } ) )
 		.pipe( sourcemaps.write() )
 		.pipe( gulp.dest( './js/' ) );
