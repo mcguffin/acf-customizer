69679fbec60d3e483c343233bcdc110e3fdb433f
mcguffin <joern@flyingletters.com>
2021-08-25 11:03:08 +0200

    fix npm

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,13 @@ const browserify	= require( 'browserify' );
 const babelify		= require( 'babelify' );
 const buffer		= require( 'vinyl-buffer' );
 const sourcemaps	= require( 'gulp-sourcemaps' );
-/*
-const sass			= require( 'gulp-sass' );
-/*/
-const sass			= require( '@selfisekai/gulp-sass' );
-//*/
+const sass			= require( 'gulp-sass' )( require('sass') );
 const source		= require( 'vinyl-source-stream' );
 const uglify		= require( 'gulp-uglify' );
 const es			= require( 'event-stream' );
 
 const package = require( './package.json' );
-sass.compiler = require('node-sass');
+
 const config = {
 	sass : {
 		outputStyle: 'compressed',
